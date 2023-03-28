682dc8d4ab0de01cb744622997c9917859eeb544
jayathungek <jayathunge.work@gmail.com>
2023-03-28 14:36:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# quick-utils
+
+This is a collection of standalone scripts (bash, python, whatever) that I've
+found myself repeatedly having to write for certain tasks. 
+
+
+
+## Index
+|Script name |Description|
+|:-----------|:-----------|
+|`dload_gdrive.sh` | Downloads large (public!!) files from Google drive via commandline. Useful for when your environment has no GUI/Browser.|
---
+++ dload_gdrive.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FILEID=""
+DEST=""
+
+print_usage(){
+	help_text=$"OPTIONS\n-i ID of the Google Drive file to download (required)\n-d download destination (required)\n"
+	printf "$help_text"
+}
+
+while getopts "hi:d:" flag; do
+	case "${flag}" in
+		h) print_usage; exit ;;
+		i) FILEID="${OPTARG}" ;;
+		d) DEST="${OPTARG}" ;;
+		:) echo "Missing option argument for -$OPTARG"; exit 1;;
+		*) print_usage; exit 1 ;;
+	esac
+done
+
+if [ -z "$FILEID" ]; then
+   echo "Error: missing argument -i"
+   print_usage; exit; 
+fi
+
+if [ -z "$DEST" ]; then
+   echo "Error: missing argument -d"
+   print_usage; exit; 
+fi
+
+wget --load-cookies /tmp/cookies.txt \
+"https://docs.google.com/uc?export=download&confirm=$(wget --quiet
+--save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate
+'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn
+'s/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $DEST && rm -rf /tmp/cookies.txt
